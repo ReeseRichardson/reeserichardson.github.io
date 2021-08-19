d155d267f5a40ecd47383dbe570f8306d1497c74
ReeseRichardson <87038325+ReeseRichardson@users.noreply.github.com>
2021-08-19 14:02:55 -0500

    huge update and clear previous commits

---
+++ README.md
@@ -0,0 +1,13 @@
+# reeserichardson.github.io
+I created this website to showcase the engineering projects I have worked on throughout my (relatively short) existence, to very generally document my life, and to demonstrate to curious individuals who I am as a person.
+
+https://reeserichardson.github.io
+
+# Contact
+If you are interested in contacting me about this website, or any other nerd stuff, please use the built in form found on the website.  Thanks!
+
+# Source Code
+Please refer to the "source_code.txt" file to see the various source code resources used for this project.
+
+# LICENSE
+Copyright (c) 2021 Reese Richardson | All Rights Reserved.
---
+++ main.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+  // stretch header across entire screen
+  var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+  }
+  ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
+  })
+
+  // parallax
+  window.addEventListener('scroll', function(e) {
+    const target = document.querySelectorAll('.parallax');
+    
+    var index = 0, length = target.length;
+    for (index; index < length; index++) {
+
+      var posX = window.pageYOffset * target[index].dataset.ratex;
+      var posY = window.pageYOffset * target[index].dataset.ratey;
+
+      if (posX >= window.innerWidth*0.5) {
+        posX = window.innerWidth*0.5;
+      }
+
+      if (posY >= window.innerHeight) {
+        posY = window.innerHeight;
+      }
+
+      target[index].style.transform = 'translate3d(' +posX+ 'px,' +posY+ 'px, 0px)';
+    }
+  });
+
+  //  hide navbar on scroll down, show navbar on scroll up w3schools (will replace w3schools code soon)
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("main-navbar").style.top = "0";
+    } 
+    
+    else {
+      document.getElementById("main-navbar").style.top = "-90px";
+    }
+
+    prevScrollpos = currentScrollPos;
+  }
+
+  // smooth scrolling
+  $("a").on("click", function (e) {
+
+    e.preventDefault();
+
+    const href = $(this).attr("href");
+
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 2000);
+  });
+
+  // current date and time
+  var clockElement = document.getElementById('clock');
+
+  function clock() {
+      clockElement.textContent = new Date().toString();
+  }
+
+  setInterval(clock);
+});
